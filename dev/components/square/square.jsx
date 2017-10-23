4b9dcf4e311f55ab16174979e20803494bfca27f
Andrii Sorokin <ansolaris@gmail.com>
2017-10-23 22:46:31 +0300

    Add navigation by arrow keys

--- dev/components/square/square.jsx
+++ dev/components/square/square.jsx
@@ -16,6 +16,38 @@ export default class Square extends React.Component {
       }).isRequired,
       onPick: PropTypes.func.isRequired,
     };
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  handleKeyDown(event) {
+    if (event.keyCode > 36 && event.keyCode < 41) {
+      const { file, rank } = this.props;
+      let nf = file;
+      let nr = rank;
+      switch (event.keyCode) {
+        case 37:
+          nf = file - 1;
+          break;
+        case 38:
+          nr = rank + 1;
+          break;
+        case 39:
+          nf = file + 1;
+          break;
+        case 40:
+          nr = rank - 1;
+          break;
+        default:
+          break;
+      }
+      if (nf > 7) nf = 0;
+      if (nf < 0) nf = 7;
+      if (nr > 7) nr = 0;
+      if (nr < 0) nr = 7;
+      const selector = `.square[data-file="${nf}"][data-rank="${nr}"]`;
+      const elemNext = document.querySelector(selector);
+      if (elemNext.focus) elemNext.focus();
+    }
   }
 
   render() {
@@ -27,20 +59,26 @@ export default class Square extends React.Component {
     } = this.props;
     let mc = '';
     let sc = '';
+    let pc = '';
     const cc = `square square_${square.color}`;
-    const pc = `square_${square.piece.type}_${square.piece.color}`;
+    if (square.piece.type) {
+      pc = ` square_${square.piece.type}_${square.piece.color}`;
+    }
     if (square.marked) {
       mc = ` square_marked_${square.color}`;
     }
     if (square.selected) {
       (sc = ' square_selected');
     }
-    const classes = `${cc} ${pc}${mc}${sc}`;
+    const classes = `${cc}${pc}${mc}${sc}`;
     return (
       <button
         className={classes}
+        data-file={file}
+        data-rank={rank}
         onMouseDown={() => onPick(file, rank)}
-        onKeyPress={() => onPick(file, rank)}
+        onClick={() => onPick(file, rank)}
+        onKeyDown={this.handleKeyDown}
       />
     );
   }
